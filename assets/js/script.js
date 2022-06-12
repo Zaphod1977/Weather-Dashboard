8cccc725a8c83f29834c573bf9edea3dbfbf544a
carfiguru@hotmail.com <carfiguru@hotmail.com>
2022-06-12 17:07:47 -0500

    js build items to start code with

---
+++ assets/js/script.js
@@ -0,0 +1,40 @@
+// form inputs to search for a city
+
+
+// current & future conditions for that city
+
+
+// city is added to the search history
+
+
+//  I get weather conditions for that city
+
+
+/* data presented {
+    city name 
+    date
+    icon representation of weather conditions
+    temperature
+    humidity
+    wind speed
+    UV index
+} */
+
+
+/* from the UV index I'll apply a class to indicate {
+    favorable
+    moderate 
+    severe
+} */
+
+
+/* I'll build a 5-day forecast for {
+    date 
+    icon representation of weather conditions
+    temperature 
+    wind speed 
+    humidity
+} */
+
+
+// When you click on a city in the search history present the data again
